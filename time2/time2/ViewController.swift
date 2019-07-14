491a7b942892cf7803445aa5126c964cd061f9b3
Arthur <atelles96@gmail.com>
2019-07-14 00:52:53 -0300

    Add: Xcode project created.

---
+++ time2/time2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  time2
+//
+//  Created by Arthur Telles on 14/07/19.
+//  Copyright © 2019 Arthur Telles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
